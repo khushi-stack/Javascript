b210cf48845e01e555ac2aa60d47f8cddfca3ef3
khushi08 <84787614+khushi-stack@users.noreply.github.com>
2024-03-03 18:21:15 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 134567
+let accountEmail = "khushi08@gmail.com"
+var accountPassword = "233455"
+accountCity = "Indore"
+let accountState;
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "345566"
+accountCity = "Ratlam"
+
+// nowadays we dont use var , use only constant and let
+
+console.table([accountEmail, accountId , accountPassword])
+
+console.log(accountState)
