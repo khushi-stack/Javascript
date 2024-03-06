3cb0c0da7f03a6a4feb54f093b79c6a288232eaf
khushi08 <84787614+khushi-stack@users.noreply.github.com>
2024-03-06 10:53:04 +0000

    container config done

--- basics/data_types.js
+++ basics/data_types.js
@@ -4,3 +4,16 @@ let loggedIn = true
 
 
 console.log(typeof age)
+
+
+const score = 100
+const outsideTemp = null
+let userEmail; //undefined
+
+const id = Symbol('123') // return datatype symbol only
+const anotherId = Symbol("123")
+console.log(id == anotherId);
+
+const BigNumber = 67785943n
+ 
+
